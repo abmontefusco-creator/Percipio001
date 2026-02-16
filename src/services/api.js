fd65ad5bd6ae1005f623902e6fe0c8ec7c75df8f
abmontefusco-creator <a.b.montefusco@gmail.com>
2026-02-16 13:56:00 +0100

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 export async function fetchTenantData(tenantId) {
-  const response = await fetch(`http://localhost:5000/api/data/${tenantId}`);
+  const response = await fetch(`https://percipio001.onrender.com/api/data/${tenantId}`);
   if (!response.ok) throw new Error("Errore nel recupero dati");
   return await response.json();
 }
